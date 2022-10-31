3cf92646254d7f8c24686ed5ff89608d1e6717a8
Ryan Hang <ryanhang92802@gmail.com>
2022-10-31 12:38:57 -0700

    forgot to add headers for file

--- filterFiles/filter.py
+++ filterFiles/filter.py
@@ -2,25 +2,31 @@
 import csv
 import re
 import sys
+import os
 
 def readOrphanFile(file,regex):
     listOfFilteredOrphans = []
     with open(file) as csvFile:
         reader = list(csv.reader(csvFile, delimiter=','))
         for row in reader[2:]:
-            if(re.search(regex, row[0])):
+            if(re.search(regex, row[0]) or row[0] == "Path"):
                 listOfFilteredOrphans.append(row)
     return listOfFilteredOrphans
 
-# create 2 new csv files that will either be a document or an image
+def writeFile(fileName,list, arg):
+    with open(fileName+'.csv', arg) as csvFile:
+            writer = csv.writer(csvFile, delimiter=',')
+            for row in list:
+               writer.writerow(row)
 def writeFilterCSV(list, fileName):
-    with open(fileName+'.csv', 'a') as csvFile:
-        writer = csv.writer(csvFile, delimiter=',')
-        for row in list:
-            writer.writerow(row)
+    if(os.path.exists(f"{fileName}.csv")):
+        writeFile(fileName,list,'w')
+    else:
+        writeFile(fileName,list,'x')
 
 def filterFiles(csvFile):
     images = readOrphanFile(csvFile, "([^\s](?=\.(jpg|jpeg|png|gif|bmp)))")
+    print(images)
     documents = readOrphanFile(csvFile,"([^\s](?=\.(pdf|doc|docx|xls|ppt|pptx|xlsx)))")
     writeFilterCSV(images, 'images')
     writeFilterCSV(documents,'documents')
